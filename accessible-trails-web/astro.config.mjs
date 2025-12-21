0d3b9b0a7c741a98f196234b62c81cf17cc1df3d
Sean Mattson <seanmattson@Seans-MacBook-Air.local>
2025-12-20 23:36:53 -0600

    pushing latest version, has adsense placeholder and affiliate link placeholders

--- accessible-trails-web/astro.config.mjs
+++ accessible-trails-web/astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://accessible-hiking-trails-project.vercel.app',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap()]
 });
